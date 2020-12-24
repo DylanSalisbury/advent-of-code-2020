2a587544222bac43d723bd0d72cd8857b9f5b9f8
Dylan Salisbury <dylansalisbury@Ashleys-MacBook-Pro-2.local>
2020-12-23 23:40:10 -0800

    Unsaved changes.

--- python/24/part1.py
+++ python/24/part1.py
@@ -2,17 +2,7 @@ import util
 
 def solve(input_path):
   f = open(input_path)
-
-  # Set of (x, y) pairs identifying tiles
-  flipped = set()
-  for l in f:
-    l = l.rstrip()
-    loc = util.rel_from_str(l)
-    if loc in flipped:
-      flipped.remove(loc)
-    else:
-      flipped.add(loc)
-  return len(flipped)
+  return len(util.flipped_set(f))
 
 if __name__ == '__main__':
     print(solve('input.txt'))
--- python/24/part2_test.py
+++ python/24/part2_test.py
@@ -5,7 +5,7 @@ class Part2Test(unittest.TestCase):
 
   def test_solve(self):
     self.assertEqual(2208, part2.solve('test_input.txt'))
-    self.assertEqual(0, part2.solve('input.txt'))
+    self.assertEqual(3887, part2.solve('input.txt'))
 
 if __name__ == '__main__':
     unittest.main()
